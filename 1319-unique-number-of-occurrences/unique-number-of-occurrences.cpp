59601fbf1c5990dc05f655fbcbc89abe737d93e4
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-01-17 08:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (9.43%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>m;
-        unordered_set<int>s;
-        int n=arr.size();
-        for(int i=0;i<n;i++)
-        {
-            m[arr[i]]++;
+        // unordered_map<int,int>m;
+        // unordered_set<int>s;
+        // int n=arr.size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     m[arr[i]]++;
+        // }
+        // int mp=m.size();
+        // for(auto it:m)
+        // {
+        //     s.insert(it.second);
+        // }
+        // int st=s.size();
+        // if(mp==st)
+        // return true;
+        // else
+        // return false;
+        /*
+            Second Approch 
+        */
+        vector<int>v(2001,0);
+        for(int &i:arr){
+            v[i+1000]++;
         }
-        int mp=m.size();
-        for(auto it:m)
-        {
-            s.insert(it.second);
+        sort(v.begin(),v.end());
+        for(int i=1;i<2001;i++){
+            if(v[i]!=0 && v[i]==v[i-1]){
+                return false;
+            }
         }
-        int st=s.size();
-        if(mp==st)
         return true;
-        else
-        return false;
-
     }
 };
