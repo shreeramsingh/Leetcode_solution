790287aa59f9fdfaa95db43f70c30e3cff7685b8
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-01-27 10:57:20 +0530

    Time: 3 ms (85.39%) | Memory: 12.6 MB (25.15%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,33 +1,50 @@
 class Solution {
 public:
-    void combo(int i, int target, vector<int>& candidates,vector<int>& ds,set<vector<int>>&ans){
-        // if(i>=candidates.size()){
+    // void combo(int i, int target, vector<int>& candidates,vector<int>& ds,set<vector<int>>&ans){
+    //     // if(i>=candidates.size()){
             
-            if(target==0){
-                // sort(ds.begin(),ds.end());
-                ans.insert(ds);
-                return;
-            }
+    //         if(target==0){
+    //             // sort(ds.begin(),ds.end());
+    //             ans.insert(ds);
+    //             return;
+    //         }
             
-        // }
-        if(target<0)
+    //     // }
+    //     if(target<0)
+    //         return;
+    //     for(int it=i; it<candidates.size() && candidates[it]<=target;it++){
+    //     //if(candidates[i]<=target){
+    //         if(it==i || candidates[it]!=candidates[it-1]){
+    //         ds.push_back(candidates[it]);
+    //         combo(it+1,target-candidates[it],candidates,ds,ans);
+    //         ds.pop_back();
+    //         }
+    //     //}
+    //     }
+    //     //combo(i+1,target,candidates,ds,ans);
+    // }
+    //striver
+    void comboination(int ind,int target,vector<int>& candidates,vector<vector<int>>&ans,vector<int>&ds)
+    {
+        if(target==0){
+            ans.push_back(ds);
             return;
-        for(int it=i; it<candidates.size() && candidates[it]<=target;it++){
-        //if(candidates[i]<=target){
-            if(it==i || candidates[it]!=candidates[it-1]){
-            ds.push_back(candidates[it]);
-            combo(it+1,target-candidates[it],candidates,ds,ans);
-            ds.pop_back();
-            }
-        //}
         }
-        //combo(i+1,target,candidates,ds,ans);
+        for(int i=ind;i<candidates.size();i++){
+            if(i>ind && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            ds.push_back(candidates[i]);
+            comboination(i+1,target-candidates[i],candidates,ans,ds);
+            ds.pop_back();
+        }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        set<vector<int>>ans;
+       // set<vector<int>>ans;
         sort(candidates.begin(), candidates.end());
+        vector<vector<int>>ans;
         vector<int>ds;
-        combo(0,target,candidates,ds,ans);
-        return {ans.begin(),ans.end()};
+        comboination(0,target,candidates,ans,ds);
+        //return {ans.begin(),ans.end()};
+        return ans;
     }
 };
