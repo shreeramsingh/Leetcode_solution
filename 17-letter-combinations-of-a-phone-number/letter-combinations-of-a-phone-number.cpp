faa66af6a4c3f2738ede35dcd28c80ebf3dd2de8
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-02-01 22:10:04 +0530

    Time: 3 ms (41.34%) | Memory: 8.3 MB (5.86%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void letterCombation(int idx,string &digits,unordered_map<int,string>&m,
+    vector<string>&ans,string temp)
+    {
+        if(idx==digits.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=0;i<m[digits[idx]-'0'].size();i++){
+            letterCombation(idx+1,digits,m,ans,temp+m[digits[idx]-'0'][i]);
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string>ans;
+        if(digits.size()==0){
+            return ans;
+        }
+        string temp="";
+        unordered_map<int,string>m;
+        m[2]="abc";
+        m[3]="def"; 
+        m[4]="ghi"; 
+        m[5]="jkl";
+        m[6]="mno"; 
+        m[7]="pqrs"; 
+        m[8]="tuv"; 
+        m[9]="wxyz";
+        letterCombation(0,digits,m,ans,temp);
+        return ans;
+    }
+};
