920d23c517b58f7f6a0ad32f7df0bc147d998dc5
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-02-02 21:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (13.81%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int>ans;
-        queue<int>q;
-        for(int i=1;i<=8;i++){
-            q.push(i);
-        }
-        while(!q.empty()){
-            int t=q.front();
-            q.pop();
-            if(t>=low && t<=high){
-                ans.push_back(t);
-            }
-            int last_digit=t%10;
-            if(last_digit+1<=9){
-                q.push(t*10+(last_digit+1));
-            }
+        vector<int> allPossible = {12, 23, 34, 45, 56, 67, 78, 89,
+                          123, 234, 345, 456, 567, 678, 789,
+                          1234, 2345, 3456, 4567, 5678, 6789,
+                          12345, 23456, 34567, 45678, 56789,
+                          123456, 234567, 345678, 456789,
+                          1234567, 2345678, 3456789,
+                          12345678, 23456789,
+                          123456789};
+        
+        vector<int> ans;
+        
+        int n = allPossible.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (allPossible[i] < low) continue;
+            
+            if (allPossible[i] > high) break;
+            
+            ans.push_back(allPossible[i]);
         }
         return ans;
     }
