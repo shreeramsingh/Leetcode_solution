7f8455f06a06d617e52bccde67467d08e17c54f5
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-02-11 02:26:50 +0530

    Time: 23 ms (28.49%) | Memory: 11.8 MB (53.59%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+    int dp[1001][1001];
     bool ispali(int i,int j,string &s){
         if(i>=j) return true;
-        if(s[i]==s[j]) return ispali(i+1,j-1,s);
-        return false;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==s[j]) return dp[i][j]=ispali(i+1,j-1,s);
+        return dp[i][j]=false;
     }
     int countSubstrings(string s) {
         int n=s.size();
+        memset(dp,-1,sizeof(dp));
         int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
