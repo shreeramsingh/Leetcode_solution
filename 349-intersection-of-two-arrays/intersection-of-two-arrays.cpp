d9d56df3970a55ae48794be94997ab8797f39a5f
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-03-11 00:04:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (46.54%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,15 +1,61 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v;
-        unordered_set<int>s,s2;
-        for(auto it:nums1) s.insert(it);
-        for(int i=0;i<nums2.size();i++){
-            if(s.find(nums2[i])!=s.end()){
-                s2.insert(nums2[i]);
+        /*
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0,j=0;
+        int m=nums1.size(),n=nums2.size();
+        vector<int>ans;
+        while(i<m && j<n){
+            if(nums1[i]==nums2[j]){
+                ans.push_back(nums1[i]);
+                while(i<m-1 && nums1[i]==nums1[i+1]){
+                    i++;
+                }
+                while(i<n-1 && nums2[j]==nums2[j+1]){
+                    j++;
+                }
+                i++;
+                j++;
+            }else if(nums1[i]<nums2[j]){
+                i++;
+            }else{
+                j++;
             }
         }
-        for(auto it:s2) v.push_back(it);
-        return v;
+        return ans;
+        */
+        sort(begin(nums1), end(nums1));
+        sort(begin(nums2), end(nums2));
+        
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        vector<int> ans;
+        int i = 0, j = 0;
+        
+        while(i < m && j < n) {
+
+            if(nums1[i] == nums2[j]) {
+                ans.push_back(nums1[i]);
+                
+                while(i < m-1 && nums1[i] == nums1[i+1]) {
+                    i++;
+                }
+                while(j < n-1 && nums2[j] == nums2[j+1]) {
+                    j++;
+                }
+                i++;
+                j++;
+                
+            } else if(nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return ans;
     }
 };
