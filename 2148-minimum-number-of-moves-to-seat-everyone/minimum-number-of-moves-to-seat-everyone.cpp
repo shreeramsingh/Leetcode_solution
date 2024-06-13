15df14f711166fb6c30646d470cc7a3883f5f10f
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-06-14 00:47:37 +0530

    Time: 3 ms (91.43%) | Memory: 21 MB (17.35%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,12 +1,35 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(seats.begin(),seats.end());
-        sort(students.begin(),students.end());
-        int ans=0;
-        for(int i=0;i<seats.size();i++){
-            ans+=abs(seats[i]-students[i]);
+        int n=seats.size();
+
+        vector<int>position_seat(101,0);
+        vector<int>position_student(101,0);
+
+        for(int &pos:seats){
+            position_seat[pos]++;
         }
+        
+        for(int &pos:students){
+            position_student[pos]++;
+        }
+
+        int i=0,j=0,ans=0;
+
+        while(i<=100 && j<=100)
+        {
+            if(position_seat[i]==0) i++;
+            if(position_student[j]==0) j++;
+
+            while(i<=100 && j<=100 && position_seat[i]!=0 && position_student[j]!=0){
+                ans+=abs(i-j);
+                position_seat[i]--;
+                position_student[j]--;
+            }
+        }
+
         return ans;
+
+        
     }
 };
