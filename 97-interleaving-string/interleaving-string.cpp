518874d61594759447bd4a94b8b1f61960caa76a
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-07-19 16:55:18 +0530

    Time: 36 ms (20.86%) | Memory: 16.6 MB (35.60%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int m,n,N;
+    int t[101][101][201];
+    bool solve(int i,int j,int k,string &s1, string &s2, string &s3){
+        if(i==m && j==n && k==N){
+            return true;
+        }
+        if(k>=N){ //other string does not get consume
+            return false;
+        }
+        if(t[i][j][k]!=-1) return t[i][j][k];
+        bool result=false;
+        if(s1[i]==s3[k]){
+            result+=solve(i+1,j,k+1,s1,s2,s3);
+        }
+        if(result==true) return t[i][j][k]=result;
+        if(s2[j]==s3[k]){
+            result+=solve(i,j+1,k+1,s1,s2,s3);
+        }
+        return t[i][j][k]=result;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        
+        m=s1.size();
+        n=s2.size();
+        N=s3.size();
+        memset(t,-1,sizeof(t));
+        return solve(0,0,0,s1,s2,s3);
+    }
+};
